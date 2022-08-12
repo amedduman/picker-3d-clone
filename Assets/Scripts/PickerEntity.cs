b4005f03fc63bfcf4c074d5701ea592634cde291
amed <amedduman@protonmail.com>
2022-08-12 23:19:10 +0300

    Add picker vertical movement

--- Assets/Scripts/PickerEntity.cs
+++ Assets/Scripts/PickerEntity.cs
@@ -6,7 +6,9 @@
 
     public class PickerEntity : MonoBehaviour
     {
+        [SerializeField] float _speed = 10;
         [SerializeField] float _horizontalMoveSensitivity = 10;
+        [SerializeField] [Min(0)] float _horizontalMoveLimit = 5;
         Rigidbody _rb;
         float _mouseX;
 
@@ -15,15 +17,25 @@
             _rb = GetComponent<Rigidbody>();
         }
 
-        // Update is called once per frame
         void Update()
         {
-            _mouseX = Input.GetAxis("Mouse X");
+            if(Input.GetMouseButton(0))
+            {
+                _mouseX = Input.GetAxis("Mouse X");
+            }
+            else
+            {
+                _mouseX = 0;
+            }
         }
 
         void FixedUpdate()
         {
-            _rb.position += new Vector3(_mouseX * Time.deltaTime * _horizontalMoveSensitivity, 0, 0);
+            float x = _mouseX * Time.deltaTime * _horizontalMoveSensitivity + _rb.position.x; 
+            // _rb.position += new Vector3(_mouseX * Time.deltaTime * _horizontalMoveSensitivity, 0, _speed * Time.deltaTime);
+            x = Mathf.Clamp(x, -_horizontalMoveLimit, _horizontalMoveLimit);
+            _rb.position = new Vector3(x, _rb.position.y, _rb.position.z + _speed * Time.deltaTime);
+            // _rb.position += new Vector3(0, 0, _speed * Time.deltaTime);
         }
     }
 
