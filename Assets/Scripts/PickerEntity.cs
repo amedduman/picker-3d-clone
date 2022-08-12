551d62f4b5f3962deaffb4ff64db91398d750f2f
amed <amedduman@protonmail.com>
2022-08-12 19:16:39 +0300

    Add picker horizontal move

---
+++ Assets/Scripts/PickerEntity.cs
@@ -0,0 +1,30 @@
+﻿namespace Picker3d
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class PickerEntity : MonoBehaviour
+    {
+        [SerializeField] float _horizontalMoveSensitivity = 10;
+        Rigidbody _rb;
+        float _mouseX;
+
+        void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _mouseX = Input.GetAxis("Mouse X");
+        }
+
+        void FixedUpdate()
+        {
+            _rb.position += new Vector3(_mouseX * Time.deltaTime * _horizontalMoveSensitivity, 0, 0);
+        }
+    }
+
+}
